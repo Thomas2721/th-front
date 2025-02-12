ccf4d0437d3020ff177ed2e23301ab8743d224a6
Thomas2721 <thomasmehari2721@gmail.com>
2025-02-12 08:29:09 -0800

    Update ShowBook.jsx

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import BackButton from "../components/home/backButton";
+import BackButton from "../components/home/BackButton";
 import axios from "axios";
 
 const ShowBook = () => {
