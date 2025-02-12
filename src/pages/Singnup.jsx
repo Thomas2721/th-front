f51d426b6cbf0fbf8477b15474b6abdea4b7bffb
Thomas2721 <thomasmehari2721@gmail.com>
2025-02-12 08:51:25 -0800

    Update Singnup.jsx

--- src/pages/Singnup.jsx
+++ src/pages/Singnup.jsx
@@ -13,7 +13,7 @@ const Singnup = () => {
   const handleSingUp = () => {
     axios
       .post(
-        "https://th-back.onrender.com/user/signup",
+        "https://th-back.vercel.app/user/signup",
         {
           username,
           email,
