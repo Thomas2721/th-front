64909105edf8911da936be7220d2cbdf518d270e
Thomas2721 <thomasmehari2721@gmail.com>
2025-02-21 08:54:34 -0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleLogin = () => {
     axios
       .post(
-        "https://th-back.vercel.app/user/signin",
+        "https://th-back.onrender.com/user/signin",
         {
           username,
           password,
