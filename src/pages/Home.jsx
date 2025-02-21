c5e65fcc2241ac948880eeb6f67a322d14c08d74
Thomas2721 <thomasmehari2721@gmail.com>
2025-02-21 09:17:46 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     console.log("token ", token);
     axios
-      .get("https://th-back.onrender.com/books", {
+      .get("https://th-back-5m76.vercel.app/books", {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
